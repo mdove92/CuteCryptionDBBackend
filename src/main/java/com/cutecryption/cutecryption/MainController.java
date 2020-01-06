ec37243b0fee34f7df8138ccf0517040d1e2964c
m.dove@live.com <m.dove@live.com>
2020-01-05 16:44:05 -0800

    Initialized project with sample request object

---
+++ src/main/java/com/cutecryption/cutecryption/MainController.java
@@ -0,0 +1,71 @@
+package com.cutecryption.cutecryption;
+
+import org.bson.Document;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.*;
+import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Controller class for our template controller
+@RestController
+public class MainController {
+
+    // Private collection variable for storing contents of our template collection
+    private MongoCollection<Document> templateCollection;
+
+    // Constructor for the template controller class
+    public MainController() {
+		// Here we pass in our connection string to our mongo db cluster
+		final MongoClient mongoClient = MongoClients.create(
+				"mongodb+srv://CuteCryption:<password>@cluster0-v5biy.mongodb.net/test?retryWrites=true&w=majority");
+		final MongoDatabase database = mongoClient.getDatabase("test");
+
+		this.templateCollection = database.getCollection("templateCollection");
+	}
+
+	// Method for handling the get request to "/" route
+	@RequestMapping("/")
+	public String index() {
+		StringBuilder builder = new StringBuilder();
+
+		// This method gets all entries from  "Template Collection"
+		final FindIterable<Document> myDoc = this.templateCollection.find();
+
+		// Iterate over each entry retrieved, and add it to our return string
+		myDoc.forEach((Block<Document>) t -> {
+			builder.append("<div class=\"templateEntry\">");
+			builder.append("</br><b>Template Name</b>  </br></br>");
+			builder.append(t.getString("Name"));
+			builder.append("</br> </br><b>Template</b> </br> </br>");
+			builder.append(t.getString("Contents"));
+			builder.append("</br> </br><b>Template Creator</b> </br> </br>");
+			builder.append(t.getString("Creator"));
+			builder.append("</div></br></br></br>");
+		});
+
+		return builder.toString();
+	}
+
+	// Method for handling post calls to add templates to mongo db
+	@PostMapping("/")
+	public ResponseEntity postController(@RequestBody TemplateRequest templateRequest) {
+
+		// Call the ToDocument() method on the template request object to turn it into a Document object
+		Document requestDoc = templateRequest.ToDocument();
+		if (requestDoc != null) {
+			// If the result is non-null, insert into collection
+			this.templateCollection.insertOne(requestDoc);
+			return ResponseEntity.ok(HttpStatus.OK);
+		} else {
+			return null;
+		}
+	}
+}
---
+++ src/main/java/com/cutecryption/cutecryption/TemplateRequest.java
@@ -0,0 +1,38 @@
+package com.cutecryption.cutecryption;
+
+import org.bson.Document;
+
+import java.util.Date;
+
+// Template request class to represent the object passed into the template controller
+public class TemplateRequest {
+
+    // Template name property
+    public String TemplateName;
+
+    // Template contents property
+    public String TemplateContents;
+
+    // Template creator property
+    public String TemplateCreator;
+
+    // Template created time property
+    private final Date CreatedTime;
+
+    // Constructor for the template request object
+    public TemplateRequest() {
+        this.CreatedTime = new Date();
+    }
+
+    // Method to return a document object that contains all the values from the request object properties
+    public Document ToDocument() {
+        // If all the required fields have contents, generate a document object containing all those fields
+        if (TemplateContents != null && TemplateContents != null && TemplateCreator != null) {
+            return new Document("Contents", this.TemplateContents).append("Name", this.TemplateName).append("Creator", this.TemplateCreator)
+                    .append("Created", this.CreatedTime.toString());
+        }
+        else{
+            return null;
+        }
+    }
+}
