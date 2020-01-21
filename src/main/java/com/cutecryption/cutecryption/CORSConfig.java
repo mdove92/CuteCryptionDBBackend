88e4e0cc596424a23a57c6931feaf43a7c46fbd0
m.dove@live.com <m.dove@live.com>
2020-01-21 03:11:57 -0800

    Actually add cors config

--- src/main/java/com/cutecryption/cutecryption/CORSConfig.java
+++ src/main/java/com/cutecryption/cutecryption/CORSConfig.java
@@ -1,7 +1,11 @@
+package com.cutecryption.cutecryption;
+
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ComponentScan("com.cutecryption.cutecryption")
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
     @Override
